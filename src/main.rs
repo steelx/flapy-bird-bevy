7788bb3a4a892f7b8a759e63388ad66ba3caf2df
Ajinkya <ajinkyab1987@gmail.com>
2020-12-12 15:09:07 +0530

    updated gravity to go horizontal right

--- src/main.rs
+++ src/main.rs
@@ -29,7 +29,7 @@ fn main() {
         .add_plugins(DefaultPlugins)
         .add_plugin(RapierPhysicsPlugin)
         .add_resource(RapierConfiguration {
-            gravity: Vector2::zeros(),
+            gravity: Vector2::new(3.0, 0.),
             ..Default::default()
         })
         .add_plugin(AnimationPlugin)
@@ -46,10 +46,9 @@ fn setup_system(
     mut materials: ResMut<Assets<ColorMaterial>>,
     mut texture_atlases: ResMut<Assets<TextureAtlas>>,
 ) {
-
     let texture_handle = asset_server.load("pixels16x16.png");
     let texture_atlas = TextureAtlas::from_grid(texture_handle, Vec2::new(16.0, 16.0), 32, 32);
-    //ColorMaterialStorage need to fix texture_atlas to able to insert to a HashMap
+
     let mut material_storage = RunState {
         materials: HashMap::new(),
         texture_atlas: texture_atlases.add(texture_atlas),
