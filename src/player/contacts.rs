652d60a5d8124e58d2c6ad3a9f5cf135be677e71
Ajinkya <ajinkyab1987@gmail.com>
2020-12-23 19:36:31 +0530

    Contact events reader

--- src/player/contacts.rs
+++ src/player/contacts.rs
@@ -7,11 +7,12 @@ impl Plugin for ContactsPlugin {
     fn build(&self, app: &mut AppBuilder) {
         app
             .add_event::<ContactEvent>()
-            .add_system(contact_setup.system());
+            .add_system(contact_setup.system())
+            .add_system(player_collider_reader.system());
     }
 }
 
-struct ContactEvent {
+pub struct ContactEvent {
     to: Entity,
     from: Entity,
     damage: u32,
@@ -19,14 +20,14 @@ struct ContactEvent {
 }
 
 fn contact_setup(
-    // collision_events: ResMut<Events<ContactEvent>>,
-    player_query: Query<(&Transform, &Collider), With<Player>>,
-    collider_query: Query<(&Transform, &Collider, &Damage), Without<Player>>,
+    mut contact_events: ResMut<Events<ContactEvent>>,
+    player_query: Query<(&Transform, &Collider, Entity), With<Player>>,
+    collider_query: Query<(&Transform, &Collider, &Damage, Entity), Without<Player>>,
 ) {
     player_query
         .iter()
-        .for_each(|(transform, player_collider)| {
-            for (collider_transform, collider_sprite, collider_damage) in collider_query.iter() {
+        .for_each(|(transform, player_collider, player_entity)| {
+            for (collider_transform, collider_sprite, collider_damage, collider_entity) in collider_query.iter() {
 
                 let collision = collide(
                     transform.translation,
@@ -42,11 +43,25 @@ fn contact_setup(
 
                 //let (reflect_x, reflect_y) =
                 match collision {
-                    Collision::Left => println!("left collision {}", collider_damage.value),
+                    Collision::Left => contact_events.send(ContactEvent {
+                        to: player_entity,
+                        from: collider_entity,
+                        damage: collider_damage.value,
+                        collision,
+                    }),
                     Collision::Right => println!("right collision"),
                     Collision::Top => println!("top collision"),
                     Collision::Bottom => println!("bottom collision"),
                 };
             }
         });
+}
+
+fn player_collider_reader(
+    events: Res<Events<ContactEvent>>,
+    mut reader: Local<EventReader<ContactEvent>>,
+) {
+    for (contact_event) in reader.iter(&events) {
+        println!("player collided with an Obstacle");
+    }
 }
